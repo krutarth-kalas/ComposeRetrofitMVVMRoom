89622b55e423bd8aa57eb936e4b7e94fc859f974
KrutarthK <KrutarthK@nsdlbank.co.in>
2024-01-15 15:47:14 +0530

    commit data

--- app/src/main/java/com/example/composeretrofitmvvmroomcode/MainActivity.kt
+++ app/src/main/java/com/example/composeretrofitmvvmroomcode/MainActivity.kt
@@ -5,54 +5,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Card
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModelProvider
-import com.example.composeretrofitmvvmroomcode.model.DataClass
-import com.example.composeretrofitmvvmroomcode.model.dummyData
+import androidx.navigation.compose.rememberNavController
 import com.example.composeretrofitmvvmroomcode.ui.composable.MainContent
-import com.example.composeretrofitmvvmroomcode.ui.theme.ComposeRetrofitMvvmRoomCodeTheme
 import com.example.composeretrofitmvvmroomcode.viewmodel.CommonViewModel
 
 class MainActivity : ComponentActivity() {
+    private val mainActivityViewModel: CommonViewModel by viewModels()
     @OptIn(ExperimentalMaterial3Api::class)
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContent {
-            MainContent()
+            val navHostController = rememberNavController()
+            MainContent(this,navHostController, mainActivityViewModel)
         }
     }
 }
